82afb1aea030dbe695a2ed79afc2e73922badff5
Dmytro Berberov <d.berberov93@gmail.com>
2025-11-30 14:01:16 +0100

    added test project(HM_1)

---
+++ main.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+
+with open('index.html', 'r', encoding='utf-8') as file:
+    html_content = file.read()
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+h1_text = soup.find('h1').text
+p_text = soup.find('p').text
+
+print(h1_text)
+print(p_text)
