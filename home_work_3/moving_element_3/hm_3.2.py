de045e54756bdfc7cf1839a1ed019d6b439aea32
DmytroBerberov <d.berberov93@gmail.com>
2025-12-07 19:26:19 +0100

    fixed check in "if"

--- home_work_3/moving_element_3/hm_3.2.py
+++ home_work_3/moving_element_3/hm_3.2.py
@@ -5,9 +5,9 @@
 # [] => []
 # [12, 3, 4, 10, 8] => [8, 12, 3, 4, 10]
 
-array = []
+array = [1]
 
-if len(array) == 0:
+if len(array) <= 1:
     print(array) # or print([])
 else:
     last_number = array.pop()
@@ -19,7 +19,7 @@ else:
 
 array_v2 = [12, 3, 4, 10, 8]
 
-if len(array_v2) == 0:
+if len(array_v2) <= 1:
     print(array_v2)
 else:
     result_slice = [array_v2[-1]] + array_v2[0: - 1]
