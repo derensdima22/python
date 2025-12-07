09c5cfb96b471cd8f227c55ebf6a4f44a5a7a077
DmytroBerberov <d.berberov93@gmail.com>
2025-12-07 21:36:35 +0100

    fixed comments

--- home_work_3/calculator/calculator.py
+++ home_work_3/calculator/calculator.py
@@ -25,14 +25,14 @@ number_two_v2 = float(input("Enter a number two: "))
 
 match operation_v2:
     case "+":
-        print(f'сумма чисел: {number_one + number_two}')
+        print(f'sum numbers: {number_one_v2 + number_two_v2}')
     case "-":
-        print(f'number difference: {number_one - number_two}')
+        print(f'numbers difference: {number_one_v2 - number_two_v2}')
     case "*":
-        print(f'multiplying numbers: {number_one * number_two}')
+        print(f'multiplying numbers: {number_one_v2 * number_two_v2}')
     case "/":
-        if number_two != 0:
-            print(f'division number: {number_one / number_two}')
+        if number_two_v2 != 0:
+            print(f'division number: {number_one_v2 / number_two_v2}')
         else:
             print("you can't divide by zero")
     case _:
@@ -43,12 +43,15 @@ match operation_v2:
 
 def add(one, two):
     return one + two
+
 def subtract(one, two):
     return one - two
+
 def multiply(one, two):
     return one * two
+
 def divide(one, two):
-    return number_one / number_two if number_two != 0 else "you can't divide by zero"
+    return one / two if two != 0 else "you can't divide by zero"
 
 operation_dist = {
     "+": add,
@@ -61,8 +64,9 @@ number_one_v3 = float(input("Enter a number one: "))
 operation_v3 = input("Enter a operation '+ - * /': ")
 number_two_v3 = float(input("Enter a number two: "))
 
-if operation in operation_dist:
-    print(operation_dist[operation](number_one_v3, number_two_v3))
+operation_func = operation_dist.get(operation_v3)
+
+if operation_func:
+    print(operation_func(number_one_v3, number_two_v3))
 else:
     print('invalid operation')
-
